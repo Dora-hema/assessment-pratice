89b077581d5a457aa89cfdfd37a5f541dd84a159
A Hemachalam Dora <46192619+Dora-hema@users.noreply.github.com>
2023-01-08 01:12:17 +0530

    Update main.tf
    added variables

--- main.tf
+++ main.tf
@@ -1,1 +1,44 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("<NAME>.json")
+
+  project = "<PROJECT_ID>"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+variable "gcp_service_list" {
+    description = "The list of apis necessary for the project"
+    type        = list(string)
+    default = [
+        "compute.googleapis.com",
+        "cloudapis.googleapis.com",
+        "vpcaccess.googleapis.com",
+        "servicenetworking.googleapis.com",
+        "cloudbuild.googleapis.com",
+        "sql-component.googleapis.com",
+        "sqladmin.googleapis.com",
+        "storage.googleapis.com",
+        "secretmanager.googleapis.com",
+        "run.googleapis.com",
+        "artifactregistry.googleapis.com",
+        "redis.googleapis.com"
+    ]
+  
+  resource "google_project_service" "all" {
+    for_each                   = toset(var.gcp_service_list)
+    project                    = var.project_number   # need to change project id
+    service                    = each.key
+    disable_on_destroy = false
+}
+  
+  
 
