9b20c819591536809afeda719d249e675e850f0a
A Hemachalam Dora <46192619+Dora-hema@users.noreply.github.com>
2023-01-09 04:38:06 +0530

    Delete metadatafetch.sh
    moved to assets

--- metadatafetch.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-
-echo "Enter type of metadata"
-read my_var
-curl curl "http://metadata.google.internal/computeMetadata/v1/instance/${my_var}/?recursive=true" -H "Metadata-Flavor: Google" > metadata.json
